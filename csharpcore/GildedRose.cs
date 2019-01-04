2cc157ddd56e6ae97046e50df03ec09a42adfa50
Adam Sobczak <sobczaka92@gmail.com>
2019-01-04 12:01:48 +0100

    move sulfuras logic to beginning of loop

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -17,12 +17,13 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    continue;
+                }
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        this.QualityHelper.DecrementItemQuality(Items[i]);
-                    }
+                    this.QualityHelper.DecrementItemQuality(Items[i]);
                 }
                 else
                 {
@@ -41,10 +42,7 @@ namespace csharpcore
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
                     Items[i].SellIn = Items[i].SellIn - 1;
-                }
 
                 if (Items[i].SellIn < 0)
                 {
@@ -52,10 +50,7 @@ namespace csharpcore
                     {
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
                                 this.QualityHelper.DecrementItemQuality(Items[i]);
-                            }
                         }
                         else
                         {
