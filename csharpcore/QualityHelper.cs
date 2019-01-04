afdfe17b930a8e7df14b8be13dc9112b9c5b0c9e
Adam Sobczak <sobczaka92@gmail.com>
2019-01-04 11:57:17 +0100

    add const values

--- csharpcore/QualityHelper.cs
+++ csharpcore/QualityHelper.cs
@@ -4,14 +4,16 @@ namespace csharpcore
 {
     public class QualityHelper
     {
+        private const int MaxQuality = 50;
+        private const int MinQuality = 0;
         public void IncrementItemQuality(Item item)
         {
-            item.Quality = Math.Min(item.Quality + 1, 50);
+            item.Quality = Math.Min(item.Quality + 1, MaxQuality);
         }
 
         public void DecrementItemQuality(Item item)
         {
-            item.Quality = Math.Max(item.Quality - 1, 0);
+            item.Quality = Math.Max(item.Quality - 1, MinQuality);
         }
 
         public void ResetItemQuality(Item item)
